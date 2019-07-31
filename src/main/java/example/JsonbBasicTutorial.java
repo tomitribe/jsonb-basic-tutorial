0bf12d742d5a0f76e6333950d72f0ac6e154381d
rmonson <rmonson@tomitribe.com>
2019-07-31 09:19:14 -0500

    first commit

---
+++ src/main/java/example/JsonbBasicTutorial.java
@@ -0,0 +1,65 @@
+/**
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.json.bind.Jsonb;
+
+import org.apache.johnzon.jsonb.JohnzonBuilder;
+
+public class JsonbBasicTutorial {
+
+	public static void main(String[] args) throws IOException {
+
+		PrintWriter writer = null;
+
+		try {
+			// Read the JSON file into a java.lang.String
+			String data = new String(Files.readAllBytes(Paths.get("MOCK_DATA.json")));
+
+			// You need a instance of the Johnzon Jsonb class to marshal JSON to pojos.
+			Jsonb jsonb = new JohnzonBuilder().build();
+
+			// Convert a java.lang.String of JSON data into an List of Person type
+			List<Person> persons = jsonb.fromJson(data, new ArrayList<Person>() {
+			}.getClass().getGenericSuperclass());
+
+			// Marshal the Person POJOs back into JSON
+			String myData = jsonb.toJson(persons);
+
+			// Create a print writer for the file MY_DATA.json
+			writer = new PrintWriter("MY_DATA.json", "UTF-8");
+
+			// write JSON data to MY_DATA.json
+			writer.print(myData);
+
+		} finally {
+			if (writer != null) {
+				writer.close();
+			}
+		}
+	}
+}
---
+++ src/main/java/example/Person.java
@@ -0,0 +1,68 @@
+/**
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package example;
+
+import javax.json.bind.annotation.JsonbProperty;
+
+public class Person {
+	// BINDING FIELDS
+	private int id;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String gender;
+	private String ip_address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getIp_address() {
+		return ip_address;
+	}
+	public void setIp_address(String ip_address) {
+		this.ip_address = ip_address;
+	}
+
+}
